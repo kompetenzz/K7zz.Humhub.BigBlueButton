61e5009817a88ff63602e58b9dc3ca587d0476f5
niels-heinemann <heinemann@kompetenzz.de>
2025-07-22 05:42:31 +0000

    Update translations

---
+++ messages/nl/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    '<strong>Bigbluebutton</strong> Integration' => '',
+    'Add navigation item' => '',
+    'Navigation item title' => '',
+    'On this page you can configure container settings of your Bigbluebutton integration.' => '',
+    'On this page you can configure general settings of your Bigbluebutton integration.' => '',
+    'Server URL' => '',
+    'Settings' => '',
+    'Shared Secret' => '',
+];
